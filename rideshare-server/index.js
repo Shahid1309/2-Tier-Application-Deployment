1d1a86a45fcacc58c6814623795400f0d9e80998
Mohd Shahid <shahiid1309@gmail.com>
2025-12-18 12:02:39 +0530

    Update backend logic and routes

--- rideshare-server/index.js
+++ rideshare-server/index.js
@@ -21,7 +21,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('*', cors());
+app.options('/*', cors());
 
 // 2️⃣ Body parser
 app.use(express.json());
